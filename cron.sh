db97c1d9642a0475950480b93a9d8fd09eed1e0b
EC2 Default User <ec2-user@ip-172-31-19-246.us-east-2.compute.internal>
2024-09-24 23:45:43 +0000

    Actualización automática

--- cron.sh
+++ cron.sh
@@ -3,11 +3,15 @@
 # Cambia al directorio del repositorio
 cd /home/ec2-user/my-server-minecraft/ || exit
 
+# Realiza un pull para asegurarte de tener la última versión
+git pull origin main 2>> log.txt  # Asegúrate de especificar la rama correcta
+
 # Agrega todos los cambios
 git add .
 
 # Realiza el commit
-git commit -m "Actualización automática"
+git commit -m "Actualización automática" 2>> log.txt
 
 # Realiza el push
-git push
+git push 2>> log.txt
+
